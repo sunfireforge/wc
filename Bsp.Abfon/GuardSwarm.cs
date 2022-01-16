ad71cb4221f4f341dea24255d0a97f0a79f00473
Josh Zampino <joshzampino@hotmail.com>
2022-01-15 18:03:13 -0800

    swarm stuff

---
+++ Bsp.Abfon/GuardSwarm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bsp.Abfon
+{
+    public class GuardSwarm : NSwarm
+    {
+        NRoleComp roleComp;
+
+        public GuardSwarm()
+        {
+            roleComp = new NRoleComp
+            {
+                Attack = Size.Small,
+                Defend = Size.Massive,
+            };
+        }
+    }
+}
